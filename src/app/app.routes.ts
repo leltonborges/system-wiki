cc4d9217101e5dda9494ef1ca92183d3670d9d8c
Lelton Crazy <leltondev@gmail.com>
2024-10-19 23:48:53 -0300

    cleaning

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
-import { ArticleCKEditorComponent } from '@c/articles/article-ckeditor/article-ckeditor.component';
 import { articlesPageResolver } from '@c/articles/resolver/articles.page.resolver';
 import { ArticleShowComponent } from '@c/articles/article-show/article-show.component';
 import { articleIdResolver } from '@c/articles/resolver/article.id.resolver';
 import { ArticleNewComponent } from '@c/articles/article-new/article-new.component';
-import { ArticleEditorComponent } from '@c/articles/article-editor/article-editor.component';
 import { ArticleListComponent } from '@c/articles/article-list/article-list.component';
 import { InitComponent } from './init.component';
 import { routerParamsGuard } from '@c/articles/guard/router-params.guard';
@@ -19,14 +17,6 @@ export const routes: Routes = [
   {
     path: 'article/new',
     component: ArticleNewComponent
-  },
-  {
-    path: 'article/simple',
-    component: ArticleCKEditorComponent
-  },
-  {
-    path: 'article/word',
-    component: ArticleEditorComponent
   },
   {
     path: 'article/list',
