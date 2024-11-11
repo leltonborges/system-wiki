2a797e4742419d2b9f2dc4018c7480204e53c42b
Lelton Borges <48037739+leltonborges@users.noreply.github.com>
2024-11-11 18:09:28 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -18,5 +18,5 @@ import 'angular-server-side-configuration/process';
 
 export const environment = {
   production: process.env['PROD'] !== 'false',
-  API_ARTICLE_URL: process.env['API_ARTICLE_URL'] || 'http://localhost:8080'
+  API_ARTICLE_URL: process.env['API_ARTICLE_URL'] || 'https://back.leltoncrazy.com'
 };
