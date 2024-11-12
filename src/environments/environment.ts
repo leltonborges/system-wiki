a374236832dc65b40dd291081fe5bae59c327194
Lelton Crazy <leltondev@gmail.com>
2024-11-11 21:42:50 -0300

    docs: update readme

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,5 @@
 import 'angular-server-side-configuration/process';
 
-/**
- * How to use angular-server-side-configuration:
- *
- * Use process.env['NAME_OF_YOUR_ENVIRONMENT_VARIABLE']
- *
- * const stringValue = process.env['STRING_VALUE'];
- * const stringValueWithDefault = process.env['STRING_VALUE'] || 'defaultValue';
- * const numberValue = Number(process.env['NUMBER_VALUE']);
- * const numberValueWithDefault = Number(process.env['NUMBER_VALUE'] || 10);
- * const booleanValue = process.env['BOOLEAN_VALUE'] === 'true';
- * const booleanValueInverted = process.env['BOOLEAN_VALUE_INVERTED'] !== 'false';
- * const complexValue = JSON.parse(process.env['COMPLEX_JSON_VALUE]);
- *
- * Please note that process.env[variable] cannot be resolved. Please directly use strings.
- */
-
 export const environment = {
   production: process.env['PROD'] !== 'false',
   API_ARTICLE_URL: process.env['API_ARTICLE_URL'] ?? 'https://back.leltoncrazy.com'
